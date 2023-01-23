f5086dd8a19f65c3088cddfaab58111aee6e76ee
jakub <jakub@jakubs-air.home>
2023-01-23 09:50:55 +0100

    Add noindex meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "tailwindcss/tailwind.css";
 import "../src/static/global.sass";
 import { Josefin_Sans, Open_Sans } from '@next/font/google'
 import React from "react";
+import Head from "next/head";
 
 const josefin = Josefin_Sans({ subsets: ['latin'], weight: ["400", "500", "600", "700"], variable: "--font-fancy" })
 const open = Open_Sans({ subsets: ['latin'], weight: ["400", "600", "700"], variable: "--font-regular" })
@@ -10,8 +11,13 @@ const open = Open_Sans({ subsets: ['latin'], weight: ["400", "600", "700"], vari
 
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <meta name="robots" content="noindex"/>
+    </Head>
     <div className={`${josefin.variable} ${open.variable}`}>
       <Component {...pageProps} />
     </div>
+    </>
   );
 }
