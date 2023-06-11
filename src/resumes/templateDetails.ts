87f449c0df18109b7d7b70932f7543f77148198e
Solpatium <solpatium003@gmail.com>
2023-06-11 17:30:04 +0200

    Add i18n for templates

--- src/resumes/templateDetails.ts
+++ src/resumes/templateDetails.ts
@@ -1,5 +1,5 @@
 import type { TemplateId } from "./templates";
-import { useMemo } from "react";
+import { useMemo, useTransition } from "react";
 import imageEnProfession from "../../public/examples/en/professional.webp"
 import imageEnSerious from "../../public/examples/en/serious.webp"
 import imageEnLax from "../../public/examples/en/lax.webp"
@@ -8,46 +8,54 @@ import imageEnPeachy from "../../public/examples/en/peachy.webp"
 import imageEnSimple from "../../public/examples/en/simple.webp"
 import type { StaticImageData } from "next/image";
 import { ApplicationPersistentState } from "../models/v1";
+import useTranslation from "next-translate/useTranslation";
 
 type TemplateDetails = Record<TemplateId, {
     title: string,
+    description: string,
     image: StaticImageData,
     example: () => Promise<ApplicationPersistentState>
 }>;
 
-// TODO: i18n!
 export const useTemplateDetails = (): TemplateDetails => {
+    const {t} = useTranslation("common");
     return useMemo(() => ({
         professional: {
-            title: "Professional",
+            title: t("templates.professional.title"),
+            description: t("templates.professional.description"),
             image: imageEnProfession,
             example: () => import("../resumes/examples/professional").then(r => r.professionalExample),
         },
         serious: {
-            title: "Serious",
+            title: t("templates.serious.title"),
+            description: t("templates.serious.description"),
             image: imageEnSerious,
             example: () => import("../resumes/examples/serious").then(r => r.seriousExample),
         },
         lax: {
-            title: "Lax",
+            title: t("templates.lax.title"),
+            description: t("templates.lax.description"),
             image: imageEnLax,
             example: () => import("../resumes/examples/lax").then(r => r.laxExample),
 
         },
         energetic: {
-            title: "Energetic",
+            title: t("templates.energetic.title"),
+            description: t("templates.energetic.description"),
             image: imageEnEnergetic,
             example: () => import("../resumes/examples/energetic").then(r => r.energeticExample),
 
         },
         peachy: {
-            title: "Peachy",
+            title: t("templates.peachy.title"),
+            description: t("templates.peachy.description"),
             image: imageEnPeachy,
             example: () => import("../resumes/examples/peachy").then(r => r.peachyExample),
 
         },
         simple: {
-            title: "Simple",
+            title: t("templates.simple.title"),
+            description: t("templates.simple.description"),
             image: imageEnSimple,
             example: () => import("../resumes/examples/simple").then(r => r.simpleExample),
 
