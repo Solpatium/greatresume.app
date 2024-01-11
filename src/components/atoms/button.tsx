b613290d577e9735b634717b06fb231567197e19
Solpatium <solpatium003@gmail.com>
2024-01-11 20:49:21 +0100

    Make buttons more consistent

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -5,9 +5,9 @@ const common =
   "inline-flex justify-center items-center px-4 py-3 border text-sm font-medium rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors";
 const colors = {
   danger: `${common} bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 text-white `,
-  primary: `${common} rounded-xl bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 text-white`,
-  secondary: `${common} rounded-md bg-indigo-50 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`,
-  tertiary: `${common} bg-white hover:bg-gray-50 border-gray-300 focus:ring-indigo-500 text-gray-600`,
+  primary: `${common} rounded-lg bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 text-white`,
+  secondary: `${common} rounded-lg bg-indigo-50 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100`,
+  tertiary: `${common} rounded-lg bg-white hover:bg-gray-50 border-gray-300 focus:ring-indigo-500 text-gray-600`,
   ghost: "text-gray-600 font-bold p-4 rounded-xl hover:bg-gray-100 hover:text-gray-900",
 };
 
