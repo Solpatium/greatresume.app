af7ee5acacc43f038b6debce1d1636f585bdf021
Solpatium <solpatium003@gmail.com>
2024-04-21 20:19:16 +0200

    Fix list item

--- src/components/atoms/listItem.tsx
+++ src/components/atoms/listItem.tsx
@@ -2,5 +2,5 @@ import cn from "classnames";
 import { forwardRef, HTMLAttributes } from "react";
 
 export const ListItem = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>((props, ref) => {
-    return <div {...props} ref={ref} className={cn("flex rounded-md p-2 my-2 border border-gray-200 bg-white alig n-center", props.className)} />
+    return <div {...props} ref={ref} className={cn("flex rounded-md p-2 my-2 border border-gray-200", props.className)} />
 })
