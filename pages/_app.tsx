97a9284f7054e1ea59cecb1bd3e45405023f7b58
Solpatium <solpatium003@gmail.com>
2024-05-04 11:48:07 +0200

    Remove noindex

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,6 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="robots" content="noindex" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
